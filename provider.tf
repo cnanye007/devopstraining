78cbe0f43187cf9dfa3feb82a1be9e8022e4cf0d
cnanye007 <114121138+cnanye007@users.noreply.github.com>
2022-09-26 11:45:57 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,15 @@
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform-user"
+  #profile = "terraform-user"
+  AWS_ACCESS_KEY_ID = "AKIAY5V7XK53MLQTLQOA"
+  AWS_SECRET_ACCESS_KEY = "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"
+  AWS_DEFAULT_REGION = "us-east-1"
 }
-#terraform {
-  #required_providers {
-   # aws = {
-      #source  = "hashicorp/aws"
-     # version = "~> 4.0"
-   # }
- # }
-#}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+   }
+ }
+}

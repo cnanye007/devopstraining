f0a4e7dc93e57395a1e93f3ed2a48edf881ac46f
cnanye007 <114121138+cnanye007@users.noreply.github.com>
2022-09-26 11:47:31 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  #profile = "terraform-user"
-  AWS_ACCESS_KEY_ID = "AKIAY5V7XK53MLQTLQOA"
-  AWS_SECRET_ACCESS_KEY = "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"
-  AWS_DEFAULT_REGION = "us-east-1"
+  profile = "terraform-user"
 }
 terraform {
   required_providers {

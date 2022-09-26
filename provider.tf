83f75ce5ca54aaba89775bcbd42296b6ac7921c0
cnanye007 <114121138+cnanye007@users.noreply.github.com>
2022-09-26 11:31:03 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
   profile = "terraform-user"
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+#terraform {
+  #required_providers {
+   # aws = {
+      #source  = "hashicorp/aws"
+     # version = "~> 4.0"
+   # }
+ # }
+#}

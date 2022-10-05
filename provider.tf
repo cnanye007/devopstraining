5303638d8e4a6566cfda67b242a964df06ba9f61
cnanye007 <114121138+cnanye007@users.noreply.github.com>
2022-10-04 23:34:45 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  profile = "default"
+  profile = "terraform_user"
+  AWS_SHARED_CREDENTIALS_FILE = "/home/ec2-user/.aws/credentials"
 }
 terraform {
   required_providers {

4922cb00cc29c776a8420a4ed322c3f88262f278
cnanye007 <114121138+cnanye007@users.noreply.github.com>
2022-10-04 23:48:06 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform_user"
+  profile = "default"
   shared_config_files = ["/home/ec2-user/.aws/config"]
   shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-   }
- }
-}
+
